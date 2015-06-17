ea5b85be7ecbf666cbb6a286883a929378e051af
Matthew Holt <Matthew.Holt+git@gmail.com>
2015-06-17 17:19:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains the source code for the [caddyserver.com](https://caddyserver.com) web site.
+
+Feel free to fix typos or make clarifications with a pull request. If you'd like to add pages/features or make drastic changes, open an issue first.
---
+++ resources/js/home.js
@@ -0,0 +1,31 @@
+$(function() {
+
+	var $spinner = $('#spinner-item-container');
+
+	var slideCount = $('#spinner .item').length;
+	var slideIdx = 0;
+
+	setInterval(function() {
+		if (slideIdx == slideCount-1)
+		{
+			// let the last one stay an extra iteration
+			slideIdx++;
+			return;
+		}
+
+		if ((++slideIdx) < slideCount)
+		{
+			$spinner.animate({
+				left: $spinner.position().left - 200
+			}, 250);
+		}
+		else
+		{
+			$spinner.animate({
+				left: 0
+			}, 250);
+			slideIdx = 0;
+		}
+	}, 2000);
+
+});
