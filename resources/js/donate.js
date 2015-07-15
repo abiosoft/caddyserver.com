8802120fa23250030554e0dd76188cd5fd2063db
Matthew Holt <Matthew.Holt+git@gmail.com>
2015-07-15 10:06:31 -0600

    Improved Google Wallet donation link

--- resources/js/donate.js
+++ resources/js/donate.js
@@ -1,10 +1,11 @@
 $(function() {
 	$('.wallet a').click(function(event) {
-		// TEMPORARILY HARD-CODED LIKE THIS, FOR EXAMPLE
+		var name = $(this).data('name');
+		var email = $(this).data('link');
 		sweetAlert({
 			imageUrl: "/resources/images/donations/wallet.png",
-			title: "Donate to Matt",
-			text: 'Send payment to <b>Matthew.Holt@gmail.com</b><br><br>You may use the Google Wallet mobile app or <a href="https://wallet.google.com">website</a>.<br><br>Thank you!',
+			title: "Donate to "+name,
+			text: 'Send payment to <b>'+email+'</b><br><br>You may use the Google Wallet mobile app or <a href="https://wallet.google.com">website</a>.<br><br>Thank you!',
 			html: true,
 			confirmButtonText: "Done"
 		});
