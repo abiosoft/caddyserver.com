aa3044402e8ac895a0368e3e9340184debb6fc62
Abiola Ibrahim <abiola89@gmail.com>
2015-07-16 04:31:35 +0100

    Added Bitcoin donate dialog.

--- resources/js/donate.js
+++ resources/js/donate.js
@@ -11,4 +11,18 @@ $(function() {
 		});
 		return suppress(event);
 	});
+	
+	$('.bitcoin a').click(function(event) {
+		var name = $(this).data('name');
+		var link = $(this).data('link');
+		var address = $(this).data('address');
+		sweetAlert({
+			imageUrl: "/resources/images/donations/bitcoin.png",
+			title: "Donate to "+name,
+			text: '<div style="text-align:center;"> <img src="//i.imgur.com/1u8U52e.png" /> <br /> </br /> <a target="_blank" href="'+ link +'">'+address+'</a> </div>',
+			html: true,
+			confirmButtonText: "Done"
+		});
+		return suppress(event);
+	});
 });
