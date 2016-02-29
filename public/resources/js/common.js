3e91804bdc0c31f9a912aa5071001be9d2d8d860
Matthew Holt <Matthew.Holt+git@gmail.com>
2016-02-29 15:06:26 -0700

    Add mailing list signup in footer
    Also tweak top nav layout on narrower screens

--- public/resources/js/common.js
+++ public/resources/js/common.js
@@ -2,7 +2,7 @@
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+})(window,document,'script','/analytics/analytics.js','ga');
 ga('create', 'UA-86578-24', 'auto');
 ga('send', 'pageview');
 
@@ -12,6 +12,11 @@ if (typeof jQuery !== 'undefined')
 	$(function() {
 		$('nav .menu-link').click(function() {
 			$('nav').toggleClass('expanded');
+		});
+
+		// Report mailing list subscriptions to Analytics
+		$('#mc-embedded-subscribe').click(function() {
+			ga('send', 'event', 'Mailing List', 'Subscribe', 'Release Announcements');
 		});
 	});
 }
